a7d792684de4123f056b1795a519e003b036b902
Wegis Silveira <wegis.h.silveira@gmail.com>
2021-02-22 21:48:15 -0300

    Criação do projeto

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import classes from './Header.module.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className={classes['Header-container']}>
+                
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import classes from './Layout.module.css'
+
+import Header from '../Header/Header'
+
+
+
+class Layout extends Component {
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
+
+export default Layout
